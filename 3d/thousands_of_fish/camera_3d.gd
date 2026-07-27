9deb6c88bedb8fbec322d7ec2db6b3af2314ebbc
TryTwo <taolas@gmail.com>
2026-07-27 08:29:56 -0700

    Replace camera script with one from other demos

--- 3d/thousands_of_fish/camera_3d.gd
+++ 3d/thousands_of_fish/camera_3d.gd
@@ -1,123 +1,45 @@
 extends Camera3D
-# Godot's default Camera3D script with physical_keycode instead of keycode.
-
-# Modifier keys' speed multiplier
-const SHIFT_MULTIPLIER = 2.5
-const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
-
-@export_range(0.0, 1.0) var sensitivity = 0.25
-
-# Mouse state
-var _mouse_position = Vector2(0.0, 0.0)
-var _total_pitch = 0.0
-
-# Movement state
-var _direction = Vector3(0.0, 0.0, 0.0)
-var _velocity = Vector3(0.0, 0.0, 0.0)
-var _acceleration = 30
-var _deceleration = -10
-var _vel_multiplier = 10
-
-# Keyboard state
-var _w = false
-var _s = false
-var _a = false
-var _d = false
-var _q = false
-var _e = false
-var _shift = false
-var _alt = false
 
 
-func _input(event):
-	# Receives mouse motion
-	if event is InputEventMouseMotion:
-		_mouse_position = event.relative
+const MOUSE_SENSITIVITY = 0.002
+const MOVE_SPEED = 5.0
 
-	# Receives mouse button input
-	if event is InputEventMouseButton:
-		match event.button_index:
-			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
-				Input.set_mouse_mode(
-					Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE
-				)
-			MOUSE_BUTTON_WHEEL_UP: # Increases max velocity
-				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 20)
-			MOUSE_BUTTON_WHEEL_DOWN: # Decreases max velocity
-				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 20)
+var rot := Vector3()
+var velocity := Vector3()
 
-	# Receives key input
-	if event is InputEventKey:
-		match event.physical_keycode:
-			KEY_W:
-				_w = event.pressed
-			KEY_S:
-				_s = event.pressed
-			KEY_A:
-				_a = event.pressed
-			KEY_D:
-				_d = event.pressed
-			KEY_Q:
-				_q = event.pressed
-			KEY_E:
-				_e = event.pressed
-			KEY_SHIFT:
-				_shift = event.pressed
-			KEY_ALT:
-				_alt = event.pressed
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-# Updates mouselook and movement every frame
-func _process(delta):
-	_update_mouselook()
-	_update_movement(delta)
+func _input(input_event: InputEvent) -> void:
+	# Mouse look (only if the mouse is captured).
+	if input_event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		# Horizontal mouse look.
+		rot.y -= input_event.screen_relative.x * MOUSE_SENSITIVITY
+		# Vertical mouse look.
+		rot.x = clamp(rot.x - input_event.screen_relative.y * MOUSE_SENSITIVITY, -1.57, 1.57)
+		transform.basis = Basis.from_euler(rot)
+
+	if input_event.is_action_pressed(&"toggle_mouse_capture"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-# Updates camera movement
-func _update_movement(delta):
-	# Computes desired direction from key states
-	_direction = Vector3(
-		(_d as float) - (_a as float),
-		(_e as float) - (_q as float),
-		(_s as float) - (_w as float),
-	)
+func _process(delta: float) -> void:
+	var motion := Vector3(
+			Input.get_axis(&"move_left", &"move_right"),
+			0.0,
+			Input.get_axis(&"move_forward", &"move_back")
+		)
+	var motion_y: float = Input.get_axis(&"move_down", &"move_up")
 
-	# Computes the change in velocity due to desired direction and "drag"
-	# The "drag" is a constant acceleration on the camera to bring it's velocity to 0
-	var offset = _direction.normalized() * _acceleration * _vel_multiplier * delta \
-			+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
+	# Normalize motion to prevent diagonal movement from being
+	# `sqrt(2)` times faster than straight movement.
+	motion = motion.normalized()
 
-	# Compute modifiers' speed multiplier
-	var speed_multi = 1
-	if _shift:
-		speed_multi *= SHIFT_MULTIPLIER
-	if _alt:
-		speed_multi *= ALT_MULTIPLIER
-
-	# Checks if we should bother translating the camera
-	if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
-		# Sets the velocity to 0 to prevent jittering due to imperfect deceleration
-		_velocity = Vector3.ZERO
-	else:
-		# Clamps speed to stay within maximum value (_vel_multiplier)
-		_velocity.x = clamp(_velocity.x + offset.x, -_vel_multiplier, _vel_multiplier)
-		_velocity.y = clamp(_velocity.y + offset.y, -_vel_multiplier, _vel_multiplier)
-		_velocity.z = clamp(_velocity.z + offset.z, -_vel_multiplier, _vel_multiplier)
-		translate(_velocity * delta * speed_multi)
-
-
-# Updates mouse look
-func _update_mouselook():
-	# Only rotates mouse if the mouse is captured
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		_mouse_position *= sensitivity
-		var yaw = _mouse_position.x
-		var pitch = _mouse_position.y
-		_mouse_position = Vector2(0, 0)
-
-		# Prevents looking up/down too far
-		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
-		_total_pitch += pitch
-
-		rotate_y(deg_to_rad(-yaw))
-		rotate_object_local(Vector3(1, 0, 0), deg_to_rad(-pitch))
+	velocity += MOVE_SPEED * delta * (transform.basis * motion)
+	velocity += Vector3(0.0, MOVE_SPEED * delta * motion_y, 0.0)
+	velocity *= 0.85
+	position += velocity
